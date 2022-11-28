bcedef3a151de6623a6745aa4c30a86af3ab8083
Rivan20 <95005451+Rivan20@users.noreply.github.com>
2022-11-28 15:26:12 +0700

    UPDATE

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,19 @@
 <template>
-  
-  <div class="layer"></div>
-  <!-- ! Body -->
-  <a class="skip-link sr-only" href="#skip-target">Skip to content</a>
-      <div class="page-flex">
-        <!-- ! Sidebar -->
-        <SideBar/>
-        <div class="main-wrapper">
-          <!-- ! Main nav -->
-          <NavBar/>
-          <!-- ! Main -->
-          <!-- <MainTable/> -->
-         <MainAdmin/>
-          <!-- ! Footer -->
-          
-      </div>
+  <div>
+    <BerandaAdminVue />
   </div>
 </template>
 
-  
 <script>
-import SideBar from './components/SideBar.vue';
-import NavBar from './components/NavBar.vue';
-import MainAdmin from './components/MainAdmin.vue';
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import MainTable from './components/MainTable.vue';
-
-
+import BerandaAdminVue from './Page/BerandaAdmin.vue';
 export default {
-  name: "App",
   components: {
-    SideBar,
-    NavBar,
-    MainAdmin,
-    // MainTable
-}
+    BerandaAdminVue
+  }
+  
 }
 </script>
+
+<style>
+  
+</style>
---
+++ src/Page/BerandaAdmin.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="layer"></div>
+    <!-- ! Body -->
+    <a class="skip-link sr-only" href="#skip-target">Skip to content</a>
+        <div class="page-flex">
+            <!-- ! Sidebar -->
+            <SideBar/>
+            <div class="main-wrapper">
+            <!-- ! Main nav -->
+            <NavBar/>
+            <!-- ! Main -->
+            <!-- <MainTable/> -->
+            <MainAdmin/>
+            <!-- ! Footer -->
+            
+        </div>
+    </div>
+</template>
+
+    
+<script>
+// import SideBar from '..SideBar.vue';  
+// import NavBar from '.NavBar.vue';
+// import MainAdmin from '..MainAdmin.vue';
+import SideBar from '../components/SideBar.vue';
+import NavBar from '../components/NavBar.vue';
+import MainAdmin from '../components/MainAdmin.vue';
+
+export default {
+    name: "App",
+    components: {
+    SideBar,
+    NavBar,
+    MainAdmin,
+    }
+}
+</script>
